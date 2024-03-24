00da11d358ed7ad154becef8ab9aeec07549762e
Willliam L. Anderson <band@users.noreply.github.com>
2024-03-24 10:31:46 -0500

    remove status bar and editor examples

--- main.ts
+++ main.ts
@@ -28,10 +28,6 @@ export default class Findex extends Plugin {
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('findex-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("What\'s up?");
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
@@ -40,15 +36,7 @@ export default class Findex extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
