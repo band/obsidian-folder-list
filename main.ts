1b3658c6554a139e8bb5f123ccad9eb5249ab3a0
Willliam L. Anderson <band@users.noreply.github.com>
2024-04-14 14:28:33 -0500

    vscode reformatting: meh or gah

--- main.ts
+++ main.ts
@@ -3,8 +3,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as util from 'util';
 
-// Remember to rename these classes and interfaces!
-
 interface FindexSettings {
 	mySetting: string;
 	omittedPaths: string[];
@@ -15,90 +13,93 @@ const DEFAULT_SETTINGS: FindexSettings = {
 	omittedPaths: [],
 }
 
+/*
 interface FindexData {
-	  omittedPaths: string[];
+	omittedPaths: string[];
 }
 
 const DEFAULT_DATA: FindexData = {
-      omittedPaths: [],
+	omittedPaths: [],
 };
+*/
 
 export default class Findex extends Plugin {
 	settings: FindexSettings;
 	data: FindexData;
 
 	async onload() {
-	      console.log('Findex: loading plugin v' + this.manifest.version);
+		console.log('Findex: loading plugin v' + this.manifest.version);
 
-	      await this.loadData();
-	      await this.loadSettings();
-	      
+		//	      await this.loadData();
+		await this.loadSettings();
+
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			const getSortedFiles = async (dir) => {
-			    return fs.readdirSync(dir).filter(item => fs.statSync(path.join(dir,item)).isFile() && !item.startsWith('.') && !item.startsWith('idx-')).sort((a,b) => fs.statSync(path.join(dir,b)).mtime.getTime() - fs.statSync(path.join(dir,a)).mtime.getTime());
+				return fs.readdirSync(dir).filter(item => fs.statSync(path.join(dir, item)).isFile() && !item.startsWith('.') && !item.startsWith('idx-')).sort((a, b) => fs.statSync(path.join(dir, b)).mtime.getTime() - fs.statSync(path.join(dir, a)).mtime.getTime());
 			};
 
 			const dirPath = path.join(this.app.vault.adapter.basePath, this.app.workspace.getActiveFile().parent.path);
 			const findexFile = path.join(dirPath, ('idx-' + path.basename(dirPath) + '.md').toLowerCase());
 			let indexHeader = path.join(dirPath, '.indexHeading.md');
-		    // create index header if needed
-		    if (fs.existsSync(indexHeader)) {
-			fs.copyFileSync(indexHeader, findexFile);
-		    } else {
-			fs.writeFileSync(findexFile, `# A list of files in ${path.basename(dirPath)}` +'\n\n', 'utf8');
-		    }
+			// create index header if needed
+			if (fs.existsSync(indexHeader)) {
+				fs.copyFileSync(indexHeader, findexFile);
+			} else {
+				fs.writeFileSync(findexFile, `# A list of files in ${path.basename(dirPath)}` + '\n\n', 'utf8');
+			}
 
-		    getSortedFiles(dirPath)
-			    .then(files => {
-				console.log('the list of files: ',files)
-				console.log('the index file: ', findexFile);
-				
-				for(const i of Object.keys(files)) {
-//				    console.log('the file: ', files[i])
-				    fs.appendFileSync(findexFile, ` - [[${files[i]}]]  ` + '\n', (err) => {
-				        if (err) {
-					    console.error('Error writing file:', err);
-					} else {
-					    console.log('File name write successful');
+			getSortedFiles(dirPath)
+				.then(files => {
+					console.log('the list of files: ', files)
+					console.log('the index file: ', findexFile);
+
+					for (const i of Object.keys(files)) {
+						//				    console.log('the file: ', files[i])
+						fs.appendFileSync(findexFile, ` - [[${files[i]}]]  ` + '\n', (err) => {
+							if (err) {
+								console.error('Error writing file:', err);
+							} else {
+								console.log('File name write successful');
+							}
+						});
 					}
-				    });
-				}
-			    })
-			    .catch(error => console.error(error));
+				})
+				.catch(error => console.error(error));
 
 			new Notice(dirPath);
 		});
-/*
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('findex-ribbon-class');
-*/
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
+		/*
+				// Perform additional things with the ribbon
+				ribbonIconEl.addClass('findex-ribbon-class');
+		
+				// This adds a simple command that can be triggered anywhere
+				this.addCommand({
+					id: 'open-sample-modal-simple',
+					name: 'Open sample modal (simple)',
+					callback: () => {
+						new SampleModal(this.app).open();
+					}
+				});
+		*/
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-	        this.addSettingTab(new FindexSettingTab(this.app, this));
+		this.addSettingTab(new FindexSettingTab(this.app, this));
 
-	    this.registerEvent(
-		this.app.vault.on('modify', (file) => {
-		    console.log('regEvent file: ', file);
-//		    const dirPath = path.join(this.app.vault.adapter.basePath, this.app.workspace.getActiveFile().parent.path);
-		    const pPath = this.app.workspace.getActiveFile().parent.path;
-		    console.log('regEvent pPath: ',pPath);
-		    if (file.path.startsWith(pPath) || pPath === "/") {
-			console.log(`File modified in ActiveFile directory: ${file.name}`);
-		    }
-		})
-	    );
+		this.registerEvent(
+			this.app.vault.on('modify', (file) => {
+				console.log('regEvent file: ', file);
+				//		    const dirPath = path.join(this.app.vault.adapter.basePath, this.app.workspace.getActiveFile().parent.path);
+				const pPath = this.app.workspace.getActiveFile().parent.path;
+				console.log('regEvent pPath: ', pPath);
+				if (file.path.startsWith(pPath) || pPath === "/") {
+					console.log(`File modified in ActiveFile directory: ${file.name}`);
+				}
+			})
+		);
 
-	    // If the plugin hooks up any global DOM events (on parts of the app that do not belong to this plugin)
+		// If the plugin hooks up any global DOM events (on parts of the app that do not belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
@@ -130,7 +131,7 @@ class FindexSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		console.log('this.plugin ', this.plugin);
 		console.log('- plugin settings: ', this.plugin.settings);
@@ -141,27 +142,27 @@ class FindexSettingTab extends PluginSettingTab {
 		const fragment = document.createDocumentFragment();
 		const link = document.createElement('a');
 		link.href =
-		  'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#writing_a_regular_expression_pattern';
+			'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#writing_a_regular_expression_pattern';
 		link.text = 'MDN - Regular expressions';
 		fragment.append('RegExp patterns to ignore. One pattern per line. See ');
 		fragment.append(link);
 		fragment.append(' for help.');
 
-            new Setting(containerEl)
-	      .setName('Omitted folder pathname patterns')
-	      .setDesc(fragment)
-	      .addTextArea((textArea) => {
-	          textArea.inputEl.setAttr('rows', 6);
-		  textArea
-	              .setPlaceholder('^daily/\n\\.png$\nfoobar.*baz')
-//		      .setValue(this.plugin.omittedPaths.join('\n'));
-      		      .setValue(this.plugin.omittedPaths);
-		  textArea.inputEl.onblur = (e: FocusEvent) => {
-		        const patterns = (e.target as HTMLInputElement).value;
-			this.plugin.omittedPaths = patterns.split('\n');
-//			this.plugin.pruneOmittedFiles();
-			this.plugin.view.redraw();
-		  };
-	        });
+		new Setting(containerEl)
+			.setName('Omitted folder pathname patterns')
+			.setDesc(fragment)
+			.addTextArea((textArea) => {
+				textArea.inputEl.setAttr('rows', 6);
+				textArea
+					.setPlaceholder('^daily/\n\\.png$\nfoobar.*baz')
+					//		      .setValue(this.plugin.omittedPaths.join('\n'));
+					.setValue(this.plugin.omittedPaths);
+				textArea.inputEl.onblur = (e: FocusEvent) => {
+					const patterns = (e.target as HTMLInputElement).value;
+					this.plugin.omittedPaths = patterns.split('\n');
+					//			this.plugin.pruneOmittedFiles();
+					this.plugin.view.redraw();
+				};
+			});
 	}
 }
